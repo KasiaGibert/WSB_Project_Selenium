73c8844c54bbf783488b0d710be681f0cc3393bf
KasiaGibert <122930273+KasiaGibert@users.noreply.github.com>
2023-05-14 18:52:56 +0200

    Delete kurs.py

--- kurs.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.maximize_window()
-driver.get('https://taniaksiazka.pl/')
-driver.find_element(By.ID, 'inputSearch')
