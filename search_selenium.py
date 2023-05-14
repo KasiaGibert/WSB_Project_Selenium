2843a9ccfe65b79e2d52bca6f3bf5f3e2be9d1ab
Kasia Gibert <katarzyna.gibert@gmail.com>
2023-05-14 12:14:06 +0200

    Poprawki

---
+++ search_selenium.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get('https://taniaksiazka.pl/')
+search_field = driver.find_element('id', 'inputSearch')
+search_field.clear()
+search_field.send_keys('sapiens')
+time.sleep(3)
+search_button = driver.find_element('xpath', '//*[@id="search-form"]/fieldset/button')
+search_button.submit()
+driver.get_screenshot_as_file('screen.png')
+time.sleep(3)
+driver.quit()
