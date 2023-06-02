a2e069d015a8897e19860fe18efaeb3ec1c7a48d
Kasia Gibert <katarzyna.gibert@gmail.com>
2023-06-02 18:09:16 +0200

    test comment

--- log_in_selenium.py
+++ log_in_selenium.py
@@ -4,6 +4,7 @@ import time
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# test
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 driver.get('https://taniaksiazka.pl/')
